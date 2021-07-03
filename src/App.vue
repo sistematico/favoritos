93904d935e37eba50a4f0306e55907866dac254a
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-03 12:26:01 -0400

    Autocommit por GitHub: alterações: add 'src/App.vue'

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,12 @@
       </a>
 
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
+        <!-- <li><a href="#" class="nav-link px-2 link-secondary">Overview</a></li> -->
+        <!-- <li><a href="#" class="nav-link px-2 link-dark">Inventory</a></li> -->
+        <!-- <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li> -->
+        <!-- <li><a href="#" class="nav-link px-2 link-dark">Products</a></li> -->
+        <li><router-link to="/" class="nav-link px-2 link-dark">Início</router-link></li>
+        <li><router-link to="/about" class="nav-link px-2 link-dark">Sobre</router-link></li>
       </ul>
 
       <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
