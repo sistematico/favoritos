af3b4d8e6e10932606801061b431913edd8bbed6
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-03 17:40:01 -0400

    Autocommit por GitHub: alterações: add 'composer.lock' add 'vendor/composer/InstalledVersions.php' add 'vendor/composer/installed.json' add 'vendor/composer/installed.php'

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '69d1bd361ad4035d0569edc7c979ea6c86be2331',
+        'name' => 'sistematico/favoritos',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sistematico/favoritos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '69d1bd361ad4035d0569edc7c979ea6c86be2331',
+            'dev_requirement' => false,
+        ),
+    ),
+);
