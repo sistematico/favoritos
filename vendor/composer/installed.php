6e5bc135143c734c8c787d80ba92a19d81c7e8ba
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-03 21:06:01 -0400

    Autocommit por GitHub: alterações: add 'public/api.php'

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '00ba9139c8863946894a7ed8976fe703605eb756',
+        'name' => 'sistematico/favoritos',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sistematico/favoritos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '00ba9139c8863946894a7ed8976fe703605eb756',
+            'dev_requirement' => false,
+        ),
+    ),
+);
