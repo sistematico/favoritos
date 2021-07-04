8e835c303da4bad722db8c4c60f0866c4220abeb
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-07-04 12:54:01 -0400

    Autocommit por GitHub: alterações: add 'app/init.php'

--- app/init.php
+++ app/init.php
@@ -7,7 +7,7 @@ $config = parse_ini_file(dirname(__DIR__) . '/.env');
 $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'];
 
 foreach ($config as $key => $value) {
-    define($key, $value);
+    define('"' . $key . '"', '"' . $value . '"');
 }
 
 Database::config(dirname(__DIR__) . '/db/database.sqlite');
